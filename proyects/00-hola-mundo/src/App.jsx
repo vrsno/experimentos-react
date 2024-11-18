c14e2d1f2a4a3f0e45e914b50af56614704fce1e
sinname <nexxuz2017@gmail.com>
2024-11-18 17:23:23 -0500

    useState

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -1,11 +1,43 @@
 import "./App.css"
 import {TwitterFollowCard} from "./TwitterFollowCard" 
+import { useState } from "react"
 
 function App(){
+    // const formatUsername = (username) => `@${username}`
+    // const formattedUsername = (<span>@{username}</span>)
+    const ladygaga = {username: "ladygaga"}
+    const [name, setName] = useState("migelito")
 return(
     <section className="App">
-    <TwitterFollowCard username={"elonmusk"} name={"elon musk best"} />
-    <TwitterFollowCard username={"jack"} name={"jack hanma jujira"} />
+    {/* con nombre */}
+    <TwitterFollowCard username={"elonmusk"}  name={"elon musk best"} />
+
+    {/* sin nombres con la propiedad children */}
+    <TwitterFollowCard  username={"billgates"}>
+        Bill gates
+    </TwitterFollowCard>
+
+    {/* sin username para uasr desconocido por defecto */}
+    <TwitterFollowCard >
+        Barack Obama
+    </TwitterFollowCard>
+
+    {/* pasar todas las propiedades del objeto como prop. mala practica */}
+    <TwitterFollowCard {...ladygaga}>
+        ladygaga
+    </TwitterFollowCard>
+
+    {/* cambiar nombre */}
+    <TwitterFollowCard username={name}>
+        ejemplo
+    </TwitterFollowCard>
+
+    <button onClick={()=>{
+        setName("predo")
+    }}>
+        cambiar nombre
+    </button>
+
     </section>
  )
 }
--- proyects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ proyects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,6 +1,17 @@
-export function TwitterFollowCard({ username, name, isFollowing }) {
-    const imageSrc = `https://unavatar.io/${username}`;
+import { useState } from "react";
 
+export function TwitterFollowCard({ children ,username = "desconocido", name}) {
+    const [isFollowing, setIsFollowing] = useState(false);
+
+    const handleClick = ()=>{
+        setIsFollowing(!isFollowing)
+    }
+
+    const imageSrc = `https://unavatar.io/${username}`;
+    // const addAt = (username) => `@${username}`;
+    // boton cam,biar de estado
+    const text = isFollowing ? 'Siguiendo' : 'Seguir';
+    const buttonClassName = isFollowing ? "tw-followCard-button is-following" : "tw-followCard-button";
 
     return (
         <article className="tw-followCard">
@@ -11,13 +22,13 @@ export function TwitterFollowCard({ username, name, isFollowing }) {
                     alt="avatar"
                 />
                 <div className="tw-followCard-info">
-                    <p>{name}</p>
+                    <p>{children || name}</p>
                     <span>@{username}</span>
                 </div>
             </header>
             <aside>
-                <button className="tw-followCard-button">
-                    {isFollowing ? 'Dejar de seguir' : 'Seguir'}
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
                 </button>
             </aside>
         </article>
